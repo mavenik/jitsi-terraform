e3946921d588cd0f44fe77f32f52806498448ff1
Nikhil Lanjewar <nikhil010586@gmail.com>
2020-07-29 23:09:24 +0530

    Remove unused reference

--- provision-only/outputs.tf
+++ provision-only/outputs.tf
@@ -3,11 +3,6 @@ output "server" {
   value = module.dns.jitsi_fqdn
 }
 
-output "root" {
-  description = "Root domain"
-  value = module.dns.root_domain
-}
-
 output "turnserver" {
   description = "TURN Server hostname"
   value = module.dns.turn_fqdn
